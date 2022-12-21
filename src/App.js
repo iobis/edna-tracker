593f342ec42420fbfe2a5ca3768f770a7c97af72
Pieter Provoost <pieterprovoost@gmail.com>
2022-12-21 13:37:53 +0100

    prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Navbar, Container, Row, Col, Table } from "react-bootstrap";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import MarkerClusterGroup from "@changey/react-leaflet-markercluster";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "leaflet/dist/leaflet.css";
+import iconMarker from "leaflet/dist/images/marker-icon.png";
+import iconRetina from "leaflet/dist/images/marker-icon-2x.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import "@changey/react-leaflet-markercluster/dist/styles.min.css";
+import L from "leaflet";
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: iconRetina,
+  iconUrl: iconMarker,
+  shadowUrl: iconShadow,
+});
 
 function App() {
+
+  const [samples, setSamples] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://1di1zf6xed.execute-api.us-east-1.amazonaws.com/v0/samples");
+      const data = await res.json();
+      setSamples(data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">
+            eDNA expeditions sample tracking
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <MapContainer
+        id="map"
+        center={[10, 0]}
+        zoom={1}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <MarkerClusterGroup>
+          {
+            samples.map(sample => <Marker position={[sample.area.coords[1], sample.area.coords[0]]} >
+              <Popup>{sample.name}</Popup>
+            </Marker>)
+          }
+        </MarkerClusterGroup>
+
+      </MapContainer>
+
+      <Container className="mt-3 mb-3">
+        <Row>
+          <Col lg={true} className="mb-3">
+            {
+              samples.length ?
+              <Table>
+                <thead>
+                  <tr>
+                    <th>Identifier</th>
+                    <th>Collected</th>
+                    <th>Location</th>
+                    <th>Size</th>
+                    <th>Extracted</th>
+                    <th>DNA concentration</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  { samples.map((sample) => <tr key={sample.name}>
+                    <td>{sample.name}</td>
+                    <td>{sample.timespan_begin}</td>
+                    <td>{sample.area_name}</td>
+                    <td>{sample.size}</td>
+                    <td>{sample.extract ? sample.extract.created_at.substring(0, 10) : ""}</td>
+                    <td>{sample.extract ? sample.extract.concentration : ""}</td>
+                  </tr>) }
+                </tbody>
+              </Table> :
+              <p>Loading sample data...</p>
+            }
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
