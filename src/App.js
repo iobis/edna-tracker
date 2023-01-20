f615265a5fde70f7a3784b6af4e7ff3cef457b78
Pieter Provoost <pieterprovoost@gmail.com>
2023-01-20 15:30:35 +0100

    changed default base map

--- src/App.js
+++ src/App.js
@@ -67,13 +67,13 @@ function App() {
       >
         <LayersControl position="topright">
           <LayersControl.Overlay name="Marker with popup">
-          <BaseLayer checked name="OpenStreetMap">
+          <BaseLayer name="OpenStreetMap">
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
             </BaseLayer>
-            <BaseLayer name="ESRI World Imagery">
+            <BaseLayer checked name="ESRI World Imagery">
               <TileLayer
                 attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
                 url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
