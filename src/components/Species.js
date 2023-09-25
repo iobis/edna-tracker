b31c019a062fc6016c22bd926f55bb7cc8247373
Pieter Provoost <pieterprovoost@gmail.com>
2023-09-25 12:38:38 +0200

    styling fix

--- src/components/Species.js
+++ src/components/Species.js
@@ -70,47 +70,49 @@ function Species({sites}) {
       { species &&
         <Row className="mt-3">
           <Col className="mb-3">
-          <h4>Fish, mammal, and turtle species in OBIS for {site.name} <span className="ms-2 badge badge-count">{species.species.length.toLocaleString("en-US")}</span></h4>
-          <p>This is a list of fish, mammal, and turtle species observed at the site according to the Ocean Biodiversity Information System (OBIS). Data can be very incomplete for some sites. The eDNA Expeditions project is in the process of mobilizing biodiversity datasets from the World Heritage sites to improve completeness.</p>
-          <Button variant="secondary" onClick={() => setShowFeedback(true)}>Provide feedback</Button>
-          { species.species.length ?
-            <Table className="mt-3 table-sm text-sm">
-              <thead>
-                <tr className="nowrap">
-                  <th></th>
-                  <th>Phylum</th>
-                  <th>Class</th>
-                  <th>Order</th>
-                  <th>Family</th>
-                  <th>Species</th>
-                  <th>Red List</th>
-                  <th>Group</th>
-                  <th>Last observed</th>
-                </tr>
-              </thead>
-              <tbody>
-                { species.species.map((sp) => <tr key={sp.AphiaID}>
-                  <td>
-                    {sp.source_obis && <OverlayTrigger placement="top" overlay={databaseTooltip}><Database /></OverlayTrigger>}
-                    {sp.source_dna && <OverlayTrigger placement="top" overlay={ednaTooltip}><Droplet /></OverlayTrigger>}
-                  </td>
-                  <td>{sp.phylum}</td>
-                  <td>{sp.class}</td>
-                  <td>{sp.order}</td>
-                  <td>{sp.family}</td>
-                  <td>{sp.species}</td>
-                  <td><span className={redlist_classname(sp.redlist_category)}>{sp.redlist_category}</span></td>
-                  {/* <td>{sp.group}</td> */}
-                  <td>
-                    { sp.group === "fish" && <OverlayTrigger placement="top" overlay={fishTooltip}><img src="fish.svg" width="20" height="20"></img></OverlayTrigger>}
-                    { sp.group === "mammal" && <OverlayTrigger placement="top" overlay={mammalTooltip}><img src="mammal.svg" width="20" height="20"></img></OverlayTrigger>}
-                    { sp.group === "turtle" && <OverlayTrigger placement="top" overlay={turtleTooltip}><img src="turtle.svg" width="20" height="20"></img></OverlayTrigger>}
-                  </td>
-                  <td>{sp.max_year}</td>
-                </tr>) }
-              </tbody>
-            </Table> : <p>No species found.</p>
-          }
+            <h4>Fish, mammal, and turtle species in OBIS for {site.name} <span className="ms-2 badge badge-count">{species.species.length.toLocaleString("en-US")}</span></h4>
+            <p>This is a list of fish, mammal, and turtle species observed at the site according to the Ocean Biodiversity Information System (OBIS). Data can be very incomplete for some sites. The eDNA Expeditions project is in the process of mobilizing biodiversity datasets from the World Heritage sites to improve completeness.</p>
+            <Button variant="secondary" onClick={() => setShowFeedback(true)}>Provide feedback</Button>
+            <div className="mt-3">
+              { species.species.length ?
+                <Table className="mt-3 table-sm text-sm">
+                  <thead>
+                    <tr className="nowrap">
+                      <th></th>
+                      <th>Phylum</th>
+                      <th>Class</th>
+                      <th>Order</th>
+                      <th>Family</th>
+                      <th>Species</th>
+                      <th>Red List</th>
+                      <th>Group</th>
+                      <th>Last observed</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    { species.species.map((sp) => <tr key={sp.AphiaID}>
+                      <td>
+                        {sp.source_obis && <OverlayTrigger placement="top" overlay={databaseTooltip}><Database /></OverlayTrigger>}
+                        {sp.source_dna && <OverlayTrigger placement="top" overlay={ednaTooltip}><Droplet /></OverlayTrigger>}
+                      </td>
+                      <td>{sp.phylum}</td>
+                      <td>{sp.class}</td>
+                      <td>{sp.order}</td>
+                      <td>{sp.family}</td>
+                      <td>{sp.species}</td>
+                      <td><span className={redlist_classname(sp.redlist_category)}>{sp.redlist_category}</span></td>
+                      {/* <td>{sp.group}</td> */}
+                      <td>
+                        { sp.group === "fish" && <OverlayTrigger placement="top" overlay={fishTooltip}><img src="fish.svg" width="20" height="20"></img></OverlayTrigger>}
+                        { sp.group === "mammal" && <OverlayTrigger placement="top" overlay={mammalTooltip}><img src="mammal.svg" width="20" height="20"></img></OverlayTrigger>}
+                        { sp.group === "turtle" && <OverlayTrigger placement="top" overlay={turtleTooltip}><img src="turtle.svg" width="20" height="20"></img></OverlayTrigger>}
+                      </td>
+                      <td>{sp.max_year}</td>
+                    </tr>) }
+                  </tbody>
+                </Table> : <p>No species found.</p>
+              }
+            </div>
           </Col>
         </Row>
       }
